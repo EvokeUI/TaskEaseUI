c8b097d46768f6959cc54842cf8708d9e95b1c55
Joslina Inti <jinti@evoketechnologies.com>
2025-11-06 18:24:17 +0530

    login simple page

--- src/app/feature/auth/login/login.ts
+++ src/app/feature/auth/login/login.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule,MatInputModule,MatButtonModule,MatCardModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+ username: string = '';
+  password: string = '';
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('Login form submitted with', this.username, this.password);
+    // Here you’d call your authentication service etc.
+  }
 
 }
