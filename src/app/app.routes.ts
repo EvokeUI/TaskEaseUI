034e8f0ac20c9de015c54e3bdf45b3fec573abd6
Vivek Kumar <vkumar@evoketechnologies.com>
2025-11-06 15:41:49 +0530

    Added auth, user and admin module with routing feature, and added login and signup component within the auth module and also implemented the lazy loading for each module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'auth', pathMatch: 'full'
+    },
+    {
+        path: 'auth', loadChildren: () => import('./auth/auth-module').then((m) => m.AuthModule)
+    },
+    {
+        path: 'user', loadChildren: () => import('./user/user-module').then((m) => m.UserModule)
+    },
+    {
+        path: 'admin', loadChildren: () => import('./admin/admin-module').then((m) => m.AdminModule)
+    }
+];
---
+++ src/app/auth/modals/user.modal.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number,
+    name: string,
+    email: string,
+    mobile: number,
+    password: string,
+    role: 'user | admin',
+}
+
