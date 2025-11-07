fc7deddcc58a4d451286b6757adac9a4b54d5a95
Vivek Kumar <vkumar@evoketechnologies.com>
2025-11-07 12:56:38 +0530

    Change in Welcome Layout and button in Login page

--- src/app/feature/auth/login/login.ts
+++ src/app/feature/auth/login/login.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   imports: [FormsModule, CommonModule, MatInputModule,MatButtonModule,MatCardModule,MatCheckboxModule, RouterLink],
