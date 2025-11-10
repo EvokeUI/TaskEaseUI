0eec10f9c10403094ebc5c36dd67f3c03d063ea2
Sai Teja Pabba <spabba3@evoketechnologies.com>
2025-11-10 11:40:24 +0530

    created register form for signing up the user details

--- src/app/feature/auth/signup/signup.ts
+++ src/app/feature/auth/signup/signup.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
-
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule }  from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  MatCardModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
+   registerForm!: FormGroup;
+  submitted = true;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      emailId: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: this.passwordsMatchValidator('password', 'confirmPassword')
+    });
+  }
+
+  // custom validator to check that two controls match
+  passwordsMatchValidator(passwordKey: string, confirmPasswordKey: string): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+      const password = group.get(passwordKey);
+      const confirm = group.get(confirmPasswordKey);
+      if (password && confirm && password.value !== confirm.value) {
+        confirm.setErrors({ notMatching: true });
+        return { notMatching: true };
+      }
+      return null;
+    };
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    console.log('Form Value', this.registerForm.value);
+    // proceed with your logic… e.g., send to server
+  }
 
 }
