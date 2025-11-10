36822866e401db528634f27299f89ef025c48035
Vivek Kumar <vkumar@evoketechnologies.com>
2025-11-10 15:10:05 +0530

    Final Changes in Welcome page

--- src/app/layouts/welcome-layout/welcome-layout.ts
+++ src/app/layouts/welcome-layout/welcome-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AdminRoutingModule } from "../../feature/admin/admin-routing-module";
 import { RouterOutlet } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-welcome-layout',
-  imports: [AdminRoutingModule, RouterOutlet, MatCardModule],
+  imports: [AdminRoutingModule, RouterOutlet, MatCardModule, MatIconModule],
   templateUrl: './welcome-layout.html',
   styleUrl: './welcome-layout.css',
 })
