5a98bada348533d3290e37a2c8ad581e116cdb62
Vivek Kumar <vkumar@evoketechnologies.com>
2025-11-10 15:27:49 +0530

    Changes in User Service and Login page

--- src/app/core/services/user-service.ts
+++ src/app/core/services/user-service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../../feature/auth/modals/user.modal';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  private userURL = "http://localhost:3000/users";
+  constructor(private http: HttpClient){}
+
+  login(username: string, password: string): Observable<User[]>{
+    return this.http.get<User[]>(this.userURL);
+  }
+
 }
--- src/app/feature/auth/login/login.ts
+++ src/app/feature/auth/login/login.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../../core/services/user-service';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +15,22 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.css',
 })
 
-export class Login {
+export class Login implements OnInit{
+  constructor(private userService: UserService){}
 
- username: string = '';
+  username: string = '';
   password: string = '';
   submitted = false;
 
   onSubmit() {
     this.submitted = true;
     console.log('Login form submitted with', this.username, this.password);
+  }
+
+  ngOnInit(): void {
+    this.userService.login(this.username, this.password).subscribe((res) =>{
+      console.log(res);
+    });
   }
   
 }
