27c8749ddf6f52daa33bf73c5298b094dcfff921
Joslina Inti <jinti@evoketechnologies.com>
2025-11-11 15:55:50 +0530

    compoenents and shared compoenents created inside dashboard

---
+++ src/app/feature/user/components/create-tasks/create-tasks.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tasks',
+  imports: [],
+  templateUrl: './create-tasks.html',
+  styleUrl: './create-tasks.css',
+})
+export class CreateTasks {
+
+}
