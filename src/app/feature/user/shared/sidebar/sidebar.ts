b88e8ed89f32feb1ca1875f23da665433dac83df
Sai Teja Pabba <spabba3@evoketechnologies.com>
2025-11-14 17:18:44 +0530

    added routerlinks in sidebar component for user module

--- src/app/feature/user/shared/sidebar/sidebar.ts
+++ src/app/feature/user/shared/sidebar/sidebar.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,RouterModule
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  events: string[] = [];
+  opened!: boolean;
+  userId!: string | null;
+  private route = inject(ActivatedRoute);
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('id');
+
+    this.route.paramMap.subscribe(params => {
+      this.userId = params.get('id');
+    });
+  }
 
 }
